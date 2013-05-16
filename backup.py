8ad148d260adbc3c5a94cf2302468e0c21e9bf60
Espen Jervidalo <espen@wasp.lan>
2013-05-16 21:48:17 +0200

    added stream size calculation and destination pool size check

--- backup.py
+++ backup.py
@@ -3,9 +3,10 @@
 import optparse
 import argparse
 import subprocess
-import logging
 import StringIO
 import shlex
+import logging
+from logging.handlers import SysLogHandler
 from datetime import datetime, timedelta
 
 # property used to check if auto updates should be made or not
@@ -31,11 +32,16 @@ ZFS_CREATE_SNAPSHOT = "/sbin/zfs snapshot {}".format
 ZFS_SEND = "/sbin/zfs send {}".format
 ZFS_SEND_INCREMENTAL = "/sbin/zfs send -i {} {}".format
 ZFS_RECEIVE = "/sbin/zfs recv {}/{}".format
+ZFS_STREAM_SIZE = "/sbin/zfs send -nP {}".format
+ZFS_INCREMENTAL_STREAM_SIZE = "/sbin/zfs send -nP -i {} {}".format
+ZFS_AVAILABLE_SPACE = "/sbin/zfs get -o value -Hp available {}".format
 
 global options
+global logger
 
 def main():
     global options
+    global logger
     usage = "usage: %prog [options]"
     parser = argparse.ArgumentParser(description='Process benchmarks.')
     
@@ -52,15 +58,19 @@ def main():
     try:
         loglevel = getattr(logging, options.log.upper())
     except AttributeError:
-        loglevel = {3:logging.CRITICAL,
-                    2:logging.ERROR,
-                    1:logging.WARNING,
-                    0:logging.INFO,
-                    -1:logging.DEBUG,
-                    }[int(options.log)]
+        loglevel = { 3:logging.CRITICAL,
+                     2:logging.ERROR,
+                     1:logging.WARNING,
+                     0:logging.INFO,
+                     -1:logging.DEBUG,
+                   } [int(options.log)]
 
     logging.basicConfig(level=loglevel)
+    logger = logging.getLogger(__file__)
+    logger.setLevel(loglevel)
+    logger.addHandler(SysLogHandler(address = "/var/run/log"))
 
+    logger.info("test")
     for pool in options.backup:
         importPool(pool)
 
@@ -98,6 +108,14 @@ def backup(pools, backupPools):
         exportPool(backupPool)
 
 def doBackup(destPool, filesystem, snapshot):
+
+    streamSize = getStreamSize(snapshot)
+    poolSize = getFreeSpace(destPool)
+
+    if streamSize > poolSize:
+        logging.error("Size of %s stream (%s) exceeds size of pool %s (%s)", snapshot, size(streamSize), destPool, size(poolSize))
+        return 1
+    
     logging.info("creating initial backup of %s to %s", snapshot, destPool)
     start = datetime.now()
     (stdoutdata, stderrdata, returncode) = pipeCommands(ZFS_SEND(snapshot), ZFS_RECEIVE(destPool, filesystem), options.pretend)
@@ -107,10 +125,17 @@ def doBackup(destPool, filesystem, snapshot):
         return 1
     else:
         delta = datetime.now() - start
-        logging.info("Initial backup of %s to %s successful in %s", filesystem, destPool, delta)
+        logging.info("Initial backup of %s to %s successful in %s, size %s", filesystem, destPool, delta, size(streamSize))
         return 0
 
 def doIncrementalBackup(destPool, filesystem, newSnapshot, snapshot):
+    streamSize = getStreamSize(snapshot)
+    poolSize = getFreeSpace(destPool)
+
+    if streamSize > poolSize:
+        logging.error("Size of %s stream (%s) exceeds size of pool %s (%s)", snapshot, size(streamSize), destPool,  size(poolSize))
+        return 1
+
     logging.info("creating incremental backup of %s to %s based on %s", newSnapshot, destPool, snapshot)
     start = datetime.now()
     (stdoutdata, stderrdata, returncode) = pipeCommands(ZFS_SEND_INCREMENTAL(snapshot, newSnapshot), ZFS_RECEIVE(destPool, filesystem), options.pretend)
@@ -119,8 +144,30 @@ def doIncrementalBackup(destPool, filesystem, newSnapshot, snapshot):
         return 1
     else:
         delta = datetime.now() - start
-        logging.info("Incremental backup of %s to %s successful in %s", filesystem, destPool, delta)
+        logging.info("Incremental backup of %s to %s successful in %s, size %s", filesystem, destPool, delta, size(streamSize))
         return 0
+
+def getFreeSpace(pool):
+    (stdoutdata, stderrdata, returncode) = runCommand(ZFS_AVAILABLE_SPACE(pool))
+    if returncode > 0:
+        logging.error("Failed to get free space for %s", pool)
+        return None
+    else:
+        return long(stdoutdata)
+
+def getStreamSize(newSnapshot, snapshot=None):
+    size = None
+    if snapshot:
+        (stdoutdata, stderrdata, returncode) = runCommand(ZFS_INCREMENTAL_STREAM_SIZE(snapshot, newSnapshot))
+    else:
+        (stdoutdata, stderrdata, returncode) = runCommand(ZFS_STREAM_SIZE(newSnapshot))
+    if returncode > 0:
+        logging.error("Failed to get stream size for %s: %s", newSnapshot, stderrdata)
+    else:
+        output = stdoutdata if stdoutdata else stderrdata
+        size = "".join([line.split()[1] for line in StringIO.StringIO(output).readlines()[1:]])
+
+    return long(size)
 
 def createSnapshot(fs, name):
     logging.debug("creating snapshot for %s", fs)
@@ -142,18 +189,18 @@ def getPools():
     return pools
 
 def importPool(pool):
-    (stdoutdata, stderrdata, returncode) = runCommand(ZPOOL_IMPORT(pool), options.pretend)
+    (stdoutdata, stderrdata, returncode) = runCommand(ZPOOL_IMPORT(pool))
     if returncode > 0:
-        logging.info("could not import pool %s: %s", pool, stderrdata)
+        logging.debug("could not import pool %s: %s", pool, stderrdata)
     else:
-        logging.info("%s imported", pool)
+        logging.debug("%s imported", pool)
 
 def exportPool(pool):   
     (stdoutdata, stderrdata, returncode) = runCommand(ZPOOL_EXPORT(pool), options.pretend)
     if returncode > 0:
-        logging.info("could not export pool %s: %s", pool, stderrdata)
+        logging.debug("could not export pool %s: %s", pool, stderrdata)
     else:
-        logging.info("%s exported", pool)
+        logging.debug("%s exported", pool)
 
 def getFilesystems(pool):
     (stdoutdata, stderrdata, returncode) = runCommand(ZFS_LIST_FILESYSTEMS(pool))
@@ -213,6 +260,13 @@ def pipeCommands(command1, command2, pretend=False):
         process2 = subprocess.Popen(args2, stdin=process1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdoutdata, stderrdata) = process2.communicate()
         return (stdoutdata, stderrdata, process2.returncode)    
+
+def size(num):
+    for x in ['bytes','KB','MB','GB']:
+        if num < 1024.0 and num > -1024.0:
+            return "%3.1f%s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
 
 if __name__ == "__main__":
     main()
